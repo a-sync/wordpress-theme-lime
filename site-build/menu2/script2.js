2558f42fedd3ec4108f294ab65e368b67163c09d
Smith <a-sync@devs.space>
2007-01-31 23:02:01 +0200

    site build

---
+++ site-build/menu2/script2.js
@@ -0,0 +1,87 @@
+//Preload Images for MouseOver
+home = new Image();
+home.src = "../media/home2.png";
+order = new Image();
+order.src = "../media/order2.png";
+users = new Image();
+users.src = "../media/users2.png";
+help = new Image();
+help.src = "../media/help2.png";
+login = new Image();
+login.src = "../media/login2.png";
+select = new Image();
+select.src = "../media/select2.png";
+
+
+//Perform MouseOver
+function home2()
+{
+ if (!document.images) return;
+ document.home.src = "../media/home2.png";
+}
+
+function home1()
+{
+ if (!document.images) return;
+ document.home.src = "../media/home1.png";
+}
+
+function order2()
+{
+ if (!document.images) return;
+ document.order.src = "../media/order2.png";
+}
+
+function order1()
+{
+ if (!document.images) return;
+ document.order.src = "../media/order1.png";
+}
+
+function users2()
+{
+ if (!document.images) return;
+ document.users.src = "../media/users2.png";
+}
+
+function users1()
+{
+ if (!document.images) return;
+ document.users.src = "../media/users1.png";
+}
+
+function help2()
+{
+ if (!document.images) return;
+ document.help.src = "../media/help2.png";
+}
+
+function help1()
+{
+ if (!document.images) return;
+ document.help.src = "../media/help1.png";
+}
+
+function login2()
+{
+ if (!document.images) return;
+ document.login.src = "../media/login2.png";
+}
+
+function login1()
+{
+ if (!document.images) return;
+ document.login.src = "../media/login1.png";
+}
+
+function select2()
+{
+ if (!document.images) return;
+ document.select.src = "../media/select2.png";
+}
+
+function select1()
+{
+ if (!document.images) return;
+ document.select.src = "../media/select1.png";
+}
